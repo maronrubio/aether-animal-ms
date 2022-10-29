989bb4724011d0ce01ec9534ad8b01d323389222
maronrubio <maronrubio@gmail.com>
2022-10-29 18:25:02 +0800

    Initial Commit

---
+++ src/main/java/xyz/budsoftstudio/aetheranimalms/AetherAnimalMsApplication.java
@@ -0,0 +1,13 @@
+package xyz.budsoftstudio.aetheranimalms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherAnimalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherAnimalMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/budsoftstudio/aetheranimalms/AetherAnimalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.budsoftstudio.aetheranimalms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherAnimalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
